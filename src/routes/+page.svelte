5e65942fa45b8aa3171907e1f7d9b20015d5210b
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2024-07-09 10:57:45 +0200

    use ui5 web components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+	import '@ui5/webcomponents/dist/Button.js';
+</script>
+
+<ui5-button>Hello UI5 Web Components</ui5-button>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
