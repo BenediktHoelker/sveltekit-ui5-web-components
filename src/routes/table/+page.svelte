46b6e72a7367160ec33aa66340fcd7a4d4babd7a
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2024-07-20 22:18:35 +0200

    data flow

--- src/routes/table/+page.svelte
+++ src/routes/table/+page.svelte
@@ -7,11 +7,6 @@
 	import '@ui5/webcomponents/dist/Label.js';
 
 	export let data;
-
-	let inputVal;
-	function handleInput(e) {
-		inputVal = e.target.value;
-	}
 </script>
 
 <ui5-table>
@@ -26,7 +21,10 @@
 	{#each data.todos as { ID, title }}
 		<ui5-table-row>
 			<ui5-table-cell><ui5-label><b>{title}</b><br />{ID}</ui5-label></ui5-table-cell>
-			<ui5-table-cell><ui5-input on:input={handleInput} value={title}></ui5-input></ui5-table-cell>
+			<ui5-table-cell
+				><ui5-input on:input={(e) => (title = e.target.value)} value={title}
+				></ui5-input></ui5-table-cell
+			>
 			<ui5-table-cell><ui5-label>{title}</ui5-label></ui5-table-cell>
 			<ui5-table-cell
 				><ui5-label style="color: #2b7c2b"><b>{title}</b> KG</ui5-label></ui5-table-cell
