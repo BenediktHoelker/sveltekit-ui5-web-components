c1502055ae07a052aed8fb7e44a018ee9f63bf6c
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2024-08-07 08:03:39 +0000

    wip: integrate cap & sveltekit

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,12 @@ import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import prettier from 'eslint-config-prettier';
 import globals from 'globals';
+import cds from '@sap/cds/eslint.config.mjs'
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
 	js.configs.recommended,
+	...cds.recommended,
 	...svelte.configs['flat/recommended'],
 	prettier,
 	...svelte.configs['flat/prettier'],
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,12 @@
 import { todos } from './data.js';
+import cds from '@sap/cds';
 
-export function load() {
+await cds.server({in_memory: true});
+const srv = await cds.connect.to('BookshopService');
+const {Books} = srv.entities("sap.capire.bookshop");
+
+export async function load() {
+	const todos = await srv.read(Books)
 	return {
 		todos
 	};
