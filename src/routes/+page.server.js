1ccfd8f1168f8062f45c1fbf92424a5c6b127902
Hölker <b27958@eon.com>
2024-08-08 21:01:16 +0200

    configure approuter

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,11 +1,11 @@
 import { todos } from './data.js';
 import cds from '@sap/cds';
 
-const srv = await cds.connect.to('BookshopService');
-const { Books } = srv.entities('sap.capire.bookshop');
+// const srv = await cds.connect.to('BookshopService');
+// const { Books } = srv.entities('sap.capire.bookshop');
 
 export async function load() {
-	const todos = await srv.read(Books);
+	// const todos = await srv.read(Books);
 	return {
 		todos
 	};
@@ -15,6 +15,6 @@ export const actions = {
 	default: async ({ cookies, request }) => {
 		const data = await request.formData();
 		const object = { ID: data.get("ID"), title: data.get("title") };
-		await UPSERT(object).into(Books);
+		// await UPSERT(object).into(Books);
 	}
 };
